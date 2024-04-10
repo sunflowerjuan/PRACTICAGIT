1de032a341ea604c6e45a3b71f7a46cdf9e238f6
Sebastian Barajas <124843816+sunflowerjuan@users.noreply.github.com>
2024-04-10 07:35:46 -0500

    Update Clase.java

--- Clase.java
+++ Clase.java
@@ -1,6 +1,21 @@
 public class Clase {
 
-    public int carechimba;
-    public String bilbao;
-    private int arr;
+    public List<T> search(String key, String method) {
+        List<T> results = new SimpleList<>();
+        Iterator<T> iterator = iterator();
+        while (iterator.hasNext()) {
+            T ob = iterator.next();
+            try {
+                Method getNameMethod = ob.getClass().getDeclaredMethod(method);
+                String name = (String) getNameMethod.invoke(ob);
+                if (name.contains(key)) {
+                    results.add(ob);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+
 }
